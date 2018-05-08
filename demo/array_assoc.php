eea163f15fdd94eff043f6e153de74a41d05e73e
RustyJonas <rusty.letmaku28@gmail.com>
2018-05-08 19:43:14 +0200

    Associative Arrays

---
+++ demo/array_assoc.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+$number = array('juanita', 'maria', 'jose');
+
+//print_r($number);
+
+echo $number[0];
+
+echo"<br>";
+$names = array('First_name' => 'Edwin', 'Last_name' => 'Diaz');
+
+echo $names['First_name'] . " " .$names['Last_name'];
+
+?>
+
+</body>
+</html>
