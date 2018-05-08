deb64f5d46a8aa675d48802ca8b2cfb6b7913963
RustyJonas <rusty.letmaku28@gmail.com>
2018-05-08 18:25:43 +0200

    Variables in PHP

---
+++ demo/variables.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+$name = 'Edwin';
+$number = 100;
+$Number_List = 100;
+$NUMBER = 100;
+$NUMBER = 100;
+
+
+echo $name . " " .  $NUMBER;
+
+$name = "<h1> Hello </h1>";
+
+echo $name;
+?>
+
+
+</body>
+</html>
