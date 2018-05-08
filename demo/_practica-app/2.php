fc82054d9177c61530643e717cc10dbe7fd83b46
RustyJonas <rusty.letmaku28@gmail.com>
2018-05-08 19:47:06 +0200

    Practice Section 2

---
+++ demo/_practica-app/2.php
@@ -0,0 +1,15 @@
+<?php
+
+$number1 = 10;
+$number2 = 20;
+
+echo $number1 + $number2 . "<br>";
+
+$lists = array(23,24,543,234,7654,3456);
+
+$lists_assoc = array('number' => 10);
+
+echo $lists_assoc['number'] . "<br>";
+
+echo $lists[0];
+
