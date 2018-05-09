de196c22749a1425141217f2d02bcd55ac2eeb4b
RustyJonas <rusty.letmaku28@gmail.com>
2018-05-09 12:17:24 +0200

    Switch Statements in PHP

---
+++ demo/switch_statements.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+$number = 10;
+
+switch($number){
+    case 34:
+        echo "is it 34";
+        break;
+    case 37:
+        echo "is it 37";
+        break;
+    case 35:
+        echo "is it 35";
+        break;
+    case 24:
+        echo "is it 24";
+        break;
+}
+?>
+
+</body>
+</html>
