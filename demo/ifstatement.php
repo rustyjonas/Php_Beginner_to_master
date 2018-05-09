bedc9d9370aa4ae540e2e2e38d5b803d3931cbb9
RustyJonas <rusty.letmaku28@gmail.com>
2018-05-09 11:46:24 +0200

    if Statements

---
+++ demo/ifstatement.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+if(3 > 10){
+    echo "three is less than ten";
+
+} else if(4 > 5){
+    echo "of course four is less than five";
+}
+
+else{
+    echo "it is not";
+}
+
+?>
+
+
+</body>
+</html>
