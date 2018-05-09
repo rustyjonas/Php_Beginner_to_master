3c463cc32a372d39b7acd7dd027ebb243bbd5343
RustyJonas <rusty.letmaku28@gmail.com>
2018-05-09 12:26:14 +0200

    while loop in php

---
+++ demo/while_loop.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+$counter = 0;
+
+while($counter < 10){
+    echo "hell student";
+    $counter = $counter + 1;
+}
+?>
+
+</body>
+</html>
