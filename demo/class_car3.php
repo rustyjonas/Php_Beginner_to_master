71187688147bb082b5820b40fa795c9cdb9b6e19
RustyJonas <rusty.letmaku28@gmail.com>
2018-05-29 16:30:18 +0200

    Adding Properties to Our Class

--- demo/class_car3.php
+++ demo/class_car3.php
@@ -3,19 +3,29 @@
 
 class Car {
 
+    var $wheels = 4;
+    var $hood = 1;
+    var $engine = 1;
+    var $doors = 4;
+
     function MoveWheels(){
+        $this->wheels = 10;
+    }
 
-        echo "Wheels move";
-
+    function CreateDoors(){
+        $this->doors = 6;
     }
 
 }
 
 
 $bmw = new Car();
-$merces_benz = new Car();
+$truck = new Car();
 
-$bmw->MoveWheels();
-$merces_benz->MoveWheels();
+echo $bmw->wheels . "<br>";
 
+echo $truck->wheels = 10 . "<br>";
+
+$truck->CreateDoors();
+echo $truck->doors;
 
