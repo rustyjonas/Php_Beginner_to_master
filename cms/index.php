c55f5670df99bd38b573d4ea028e851244face6c
RustyJonas <rusty.letmaku28@gmail.com>
2018-05-31 16:13:36 +0200

    Inserting the post image and displaying it

--- cms/index.php
+++ cms/index.php
@@ -27,6 +27,7 @@
                     $post_date = $row['post_date'];
                     $post_image = $row['post_image'];
                     $post_content = $row['post_content'];
+
                 ?>
 
                 <h1 class="page-header">
@@ -43,7 +44,7 @@
                 </p>
                 <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date; ?></p>
                 <hr>
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
+                <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
                 <hr>
                 <p><?php echo $post_content; ?></p>
                 <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
