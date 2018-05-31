b36fd8ec16f1020e3fbc45d9bb2d42774753de46
RustyJonas <rusty.letmaku28@gmail.com>
2018-05-31 16:50:16 +0200

    Creating a custom search engine part 3

--- cms/includes/sidebar.php
+++ cms/includes/sidebar.php
@@ -1,29 +1,7 @@
 
 <div class="col-md-4">
 
-    <?php
-        include "db.php";
 
-        if(isset($_POST['submit'])){
-           $search = $_POST['search'];
-
-           $query = "SELECT * FROM posts WHERE post_tags LIKE '%$search%'";
-           $search_query = mysqli_query($connection,$query);
-
-           if(!$search_query){
-               die("QUERY FAILED" . mysqli_error($connection));
-           }
-
-           $count = mysqli_num_rows($search_query);
-
-           if($count == 0){
-               echo "<h1>NO RESULT</h1>";
-           }else{
-               echo "SOME RESULT";
-           }
-        }
-
-    ?>
 
 
     <!-- Blog Search Well -->
