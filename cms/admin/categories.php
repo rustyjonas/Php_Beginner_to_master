22f6c57ea60bf09158094c44ab762dfe9aeeaf20
RustyJonas <rusty.letmaku28@gmail.com>
2018-06-05 15:59:28 +0200

    Adding Categories

--- cms/admin/categories.php
+++ cms/admin/categories.php
@@ -20,7 +20,28 @@
                     </h1>
 
                     <div class="col-xs-6">
-                        <form action="">
+                        <?php
+                        if(isset($_POST['submit'])){
+                            $cat_title = $_POST['cat_title'];
+
+                            if($cat_title == "" || empty($cat_title)) {
+                                echo "This field should not be empty";
+
+                            } else {
+                                $query = "INSERT INTO categories (cat_title) VALUES ('{$cat_title}')";
+
+                                $create_category_query = mysqli_query($connection,$query);
+
+                                if(!$create_category_query){
+                                    die('QUERY FAILED' . mysqli_error($connection));
+                                }
+                            }
+                        }
+                        ?>
+
+
+
+                        <form method="POST" action="">
                             <div class="form-group">
                                 <label for="cat-title">Add Category</label>
                                 <input class="form-control" type="text" name="cat_title">
