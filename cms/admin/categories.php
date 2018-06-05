ff5ba7ba2107e1b95fc18619a3cf9ebce2f029f5
RustyJonas <rusty.letmaku28@gmail.com>
2018-06-05 17:19:36 +0200

    Updating or Editing Categories part 2

--- cms/admin/categories.php
+++ cms/admin/categories.php
@@ -52,32 +52,17 @@
 
                         </form>
 
-                        <form method="POST" action="">
-                            <div class="form-group">
-                                <label for="cat-title">Edit Category</label>
 
-                                <?php
-                                    if(isset($_GET['edit'])){
-                                    $cat_id = $_GET['edit'];
+                    <?php
+                    if(isset($_GET['edit'])){
+                        $cat_id = $_GET['edit'];
 
-                                $query = "SELECT * FROM categories WHERE cat_id = {$cat_id} ";
-                                $select_categories_id = mysqli_query($connection,$query);
+                        include "includes/update_categories.php";
+                    }
+
+                    ?>
 
 
-                                while($row = mysqli_fetch_assoc($select_categories_id)){
-                                    $cat_id = $row['cat_id'];
-                                    $cat_title = $row['cat_title'];
-
-                                ?>
-                                    <input value="<?php if(isset($cat_title)){echo $cat_title;}?>"
-                                           class="form-control" type="text" name="cat_title">
-                                <?php }} ?>
-                            </div>
-                            <div class="form-group">
-                                <input class="btn btn-primary" type="submit" name="submit" value="Edit">
-                            </div>
-
-                        </form>
                     </div><!-- Add Category Form -->
 
                     <div class="col-xs-6">
