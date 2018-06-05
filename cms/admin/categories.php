5e9ffc2a7a58ae78809c334c1693c3e3a8f40a0a
RustyJonas <rusty.letmaku28@gmail.com>
2018-06-05 17:02:57 +0200

    Updating or Editing Categories part 1

--- cms/admin/categories.php
+++ cms/admin/categories.php
@@ -51,12 +51,36 @@
                             </div>
 
                         </form>
+
+                        <form method="POST" action="">
+                            <div class="form-group">
+                                <label for="cat-title">Edit Category</label>
+
+                                <?php
+                                    if(isset($_GET['edit'])){
+                                    $cat_id = $_GET['edit'];
+
+                                $query = "SELECT * FROM categories WHERE cat_id = {$cat_id} ";
+                                $select_categories_id = mysqli_query($connection,$query);
+
+
+                                while($row = mysqli_fetch_assoc($select_categories_id)){
+                                    $cat_id = $row['cat_id'];
+                                    $cat_title = $row['cat_title'];
+
+                                ?>
+                                    <input value="<?php if(isset($cat_title)){echo $cat_title;}?>"
+                                           class="form-control" type="text" name="cat_title">
+                                <?php }} ?>
+                            </div>
+                            <div class="form-group">
+                                <input class="btn btn-primary" type="submit" name="submit" value="Edit">
+                            </div>
+
+                        </form>
                     </div><!-- Add Category Form -->
 
                     <div class="col-xs-6">
-
-
-
 
                         <table class="table table-bordered table-hover">
                             <thead>
@@ -78,6 +102,7 @@
                                 echo"<td>{$cat_id}</td>";
                                 echo"<td>{$cat_title}</td>";
                                 echo"<td><a href='categories.php?delete={$cat_id}'>Delete</a></td>";
+                                echo"<td><a href='categories.php?edit={$cat_id}'>Edit</a></td>";
                                 echo "</tr>";
                             }
                             ?>
@@ -92,7 +117,9 @@
                             header("Location: categories.php");
                             }
 
- ?>
+
+
+                            ?>
 
                             </tbody>
                         </table>
