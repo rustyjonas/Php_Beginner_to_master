3f86251756f04b07cd70966ddc5809efad18ff4f
RustyJonas <rusty.letmaku28@gmail.com>
2018-06-06 13:31:09 +0200

    Finally Updating Posts

--- cms/admin/includes/edit_post.php
+++ cms/admin/includes/edit_post.php
@@ -19,18 +19,48 @@ while($row = mysqli_fetch_assoc($select_posts_by_id)) {
     $post_comment_count = $row['post_comment_count'];
     $post_date = $row['post_date'];
     $post_content = $row['post_content'];
+    }
 
+    if(isset($_POST['update_post'])){
+        $post_author = $_POST['post_author'];
+        $post_title = $_POST['post_title'];
+        $post_category_id = $_POST['post_category'];
+        $post_status = $_POST['post_status'];
+        $post_image = $_FILES['image']['name'];
+        $post_image_temp = $_FILES['image']['tmp_name'];
+        $post_content = $_POST['post_content'];
+        $post_tags = $_POST['post_tags'];
 
-}
+        move_uploaded_file($post_image_temp, "../image/$post_image");
+
+        if(empty($post_image)){
+            $query = "SELECT * FROM posts WHERE post_id = {$p_id}";
+            $select_image = mysqli_query($connection,$query);
+
+            while($row = mysqli_fetch_array($select_image)){
+
+                $post_image = $row['post_image'];
+            }
+        }
+        $query = "UPDATE posts SET post_title = '{$post_title}', post_category_id = '{$post_category_id}',
+                  post_date = now(), post_author = '{$post_author}', post_status = '{$post_status}',
+                  post_tags= '{$post_tags}', post_content = '{$post_content}', post_image = '{$post_image}' 
+                  WHERE post_id = '{$post_id}'";
+
+        $update_post = mysqli_query($connection,$query);
+
+        confirmQuery($update_post);
+
+    }
+
 ?>
 
 <form style="padding-top: 7em;" action="" method="POST" enctype="multipart/form-data">
 
     <div class="form-group">
         <label for="title">Post Title</label>
-        <input type="text" class="form-control" value="<?php echo $post_title; ?>" name="title">
+        <input type="text" class="form-control" value="<?php echo $post_title; ?>" name="post_title">
     </div>
-
 
     <div class="form-group">
         <select name="post_category" id="post_category">
@@ -54,7 +84,7 @@ while($row = mysqli_fetch_assoc($select_posts_by_id)) {
 
     <div class="form-group">
         <label for="title">Post Author</label>
-        <input type="text" name="author" value="<?php echo $post_author; ?>" class="form-control">
+        <input type="text" name="post_author" value="<?php echo $post_author; ?>" class="form-control">
     </div>
 
     <div class="form-group">
@@ -69,6 +99,7 @@ while($row = mysqli_fetch_assoc($select_posts_by_id)) {
 
     <div class="form-group">
         <img width="100" src="image/<?php echo$post_image;?>" alt="">
+        <input type="file" name="image">
     </div>
 
     <div class="form-group">
