c8f55be1495418aa2fea15ae747da3f8bff9e049
RustyJonas <rusty.letmaku28@gmail.com>
2018-06-06 10:45:36 +0200

    Creating Post HTML form in admin

--- cms/admin/posts.php
+++ cms/admin/posts.php
@@ -44,8 +44,10 @@
 
                         switch($source){
 
-                            case '34':
-                                echo "NICE 34";
+                            case 'add_post':
+
+                                include "includes/add_post.php";
+
                                 break;
 
                             case '100':
