112aadee2ed56bca4af7788c570f1d4e8daccc90
RustyJonas <rusty.letmaku28@gmail.com>
2018-06-07 14:22:29 +0200

    Adding users in admin part 2

--- cms/admin/includes/add_user.php
+++ cms/admin/includes/add_user.php
@@ -3,28 +3,26 @@
 <?php
 if(isset($_POST['create_user'])){
 
-    $post_title = $_POST['title'];
-    $post_author = $_POST['author'];
-    $post_category_id = $_POST['post_category'];
-    $post_status = $_POST['post_status'];
+    echo $user_firstname = $_POST['user_firstname'];
+    $user_lastname = $_POST['user_lastname'];
+    $user_role = $_POST['user_role'];
 
-    $post_image = $_FILES['image']['name'];
-    $post_image_temp = $_FILES['image']['tmp_name'];
+//    $post_image = $_FILES['image']['name'];
+//    $post_image_temp = $_FILES['image']['tmp_name'];
 
-    $post_tags = $_POST['post_tags'];
-    $post_content = $_POST['post_content'];
-    $post_date = date('d-m-y');
+    $username = $_POST['username'];
+    $user_email = $_POST['user_email'];
+    $user_password = $_POST['user_password'];
 
-    move_uploaded_file($post_image_temp, "image/$post_image");
+//    move_uploaded_file($post_image_temp, "image/$post_image");
 
-
-    $query = "INSERT INTO posts (post_category_id, post_title, post_author, post_date, post_image, 
-                post_content, post_tags, post_status) VALUES ({$post_category_id},'{$post_title}','{$post_author}',now(),
-              '{$post_image}','{$post_content}','{$post_tags}','{$post_status}')";
-
-    $add_post_query = mysqli_query($connection,$query);
-
-    confirmQuery($add_post_query);
+//    $query = "INSERT INTO posts (post_category_id, post_title, post_author, post_date, post_image,
+//                post_content, post_tags, post_status) VALUES ({$post_category_id},'{$post_title}','{$post_author}',now(),
+//              '{$post_image}','{$post_content}','{$post_tags}','{$post_status}')";
+//
+//    $add_post_query = mysqli_query($connection,$query);
+//
+//    confirmQuery($add_post_query);
 }
 ?>
 
@@ -42,23 +40,13 @@ if(isset($_POST['create_user'])){
 
     <div class="form-group">
         <select name="user_role" id="">
-            <?php
-            $query = "SELECT * FROM users";
-            $select_users = mysqli_query($connection,$query);
-
-            confirmQuery($select_users);
-
-            while($row = mysqli_fetch_assoc($select_users)){
-                $user_id = $row['user_id'];
-                $user_role = $row['user_role'];
-
-                echo "<option value='$user_id'>{$user_role}</option>";
-            }
-            ?>
+            <option value="Subscriber">Select Options</option>
+            <option value="Admin">Admin</option>
+            <option value="Subscriber">Subscriber</option>
 
         </select>
-
     </div>
+
     <div class="form-group">
         <label for="username">Username</label>
         <input type="text" class="form-control" name="username">
@@ -73,9 +61,6 @@ if(isset($_POST['create_user'])){
         <label for="user_password">Password</label>
         <input type="password" class="form-control" name="user_password">
     </div>
-
-
-
 
     <div class="form-group">
         <input type="submit" name="create_user" class="btn btn-primary" value="Add User">
