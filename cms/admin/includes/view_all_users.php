6d85624eb2c26afc33d84112aef0e6167d75c12f
RustyJonas <rusty.letmaku28@gmail.com>
2018-06-07 13:39:52 +0200

    Displaying users in admin

--- cms/admin/includes/view_all_users.php
+++ cms/admin/includes/view_all_users.php
@@ -42,9 +42,11 @@
 
 
                         echo "<tr>";
-                        echo "<td>$comment_id</td>";
-                        echo "<td>$comment_author</td>";
-                        echo "<td>$comment_content</td>";
+                        echo "<td>$user_id</td>";
+                        echo "<td>$username</td>";
+                        echo "<td>$user_firstname</td>";
+
+
 //
 //                        $query = "SELECT * FROM categories WHERE cat_id = {$post_category_id}";
 //
@@ -55,22 +57,24 @@
 //                            $cat_title = $row['cat_title'];
 //                            echo "<td>{$cat_title}</td>";
 //                        }
-                        echo "<td>$comment_email</td>";
-                        echo "<td>$comment_status</td>";
+                        echo "<td>$user_lastname</td>";
+                        echo "<td>$user_email</td>";
+                        echo "<td>$user_role</td>";
 
-                        $query = "SELECT * FROM posts WHERE post_id = $comment_post_id";
-                        $select_post_id_query = mysqli_query($connection,$query);
-
-                        while($row = mysqli_fetch_assoc($select_post_id_query)){
-                            $post_id = $row['post_id'];
-                            $post_title = $row['post_title'];
-                            echo "<td><a href='../post.php?p_id=$post_id'>{$post_title}</a></td>";
-
-                        }
-                        echo "<td>$comment_date</td>";
-                        echo "<td><a href='comments.php?approve={$comment_id}'>Approve</a></td>";
-                        echo "<td><a href='comments.php?unapprove={$comment_id}'>Unapprove</a></td>";
-                        echo "<td><a href='comments.php?delete={$comment_id}'>Delete</a></td>";
+//
+//                        $query = "SELECT * FROM posts WHERE post_id = $comment_post_id";
+//                        $select_post_id_query = mysqli_query($connection,$query);
+//
+//                        while($row = mysqli_fetch_assoc($select_post_id_query)){
+//                            $post_id = $row['post_id'];
+//                            $post_title = $row['post_title'];
+//                            echo "<td><a href='../post.php?p_id=$post_id'>{$post_title}</a></td>";
+//
+//                        }
+                        echo "<td></td>";
+                        echo "<td><a href='comments.php?approve='>Approve</a></td>";
+                        echo "<td><a href='comments.php?unapprove='>Unapprove</a></td>";
+                        echo "<td><a href='comments.php?delete='>Delete</a></td>";
                         echo "</tr>";
                     }
                     ?>
