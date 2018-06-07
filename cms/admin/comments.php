5e993a6914a8fe3c35ece32fd3c06725a8d9a20a
RustyJonas <rusty.letmaku28@gmail.com>
2018-06-07 10:59:48 +0200

    Creating the comments page and HTML form in admin

---
+++ cms/admin/comments.php
@@ -0,0 +1,48 @@
+
+<?php include "includes/admin_header.php";?>
+
+
+<?php include "includes/admin_navigation.php"; ?>
+
+<?php
+if(isset($_GET['source'])){
+
+    $source = $_GET['source'];
+
+} else {
+    $source = '';
+}
+
+switch($source){
+
+    case 'add_post':
+        include "includes/add_post.php";
+        break;
+
+    case 'edit_post':
+        include "includes/edit_post.php";
+        break;
+
+    case '200':
+        echo "NICE 200";
+        break;
+
+    default:
+        include "includes/view_all_comments.php";
+        break;
+}
+?>
+
+<!-- /.row -->
+
+</div>
+<!-- /.container-fluid -->
+
+
+
+
+
+<!-- /#page-wrapper -->
+
+
+<?php include "includes/admin_footer.php";?>
--- cms/admin/includes/admin_navigation.php
+++ cms/admin/includes/admin_navigation.php
@@ -55,7 +55,7 @@
             </li>
 
             <li class="active">
-                <a href="blank-page.html"><i class="fa fa-fw fa-file"></i> Comments </a>
+                <a href="comments.php"><i class="fa fa-fw fa-file"></i> Comments </a>
             </li>
 
             <li>
