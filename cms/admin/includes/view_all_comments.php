695a910abef6bf46fdcc9769c1a2098c942a2d06
RustyJonas <rusty.letmaku28@gmail.com>
2018-06-07 12:26:09 +0200

    Approving and unapproving comments

--- cms/admin/includes/view_all_comments.php
+++ cms/admin/includes/view_all_comments.php
@@ -68,8 +68,8 @@
 
                         }
                         echo "<td>$comment_date</td>";
-                        echo "<td><a href='posts.php?source=edit_post&p_id='>Approve</a></td>";
-                        echo "<td><a href='posts.php?delete='>Unapprove</a></td>";
+                        echo "<td><a href='comments.php?approve={$comment_id}'>Approve</a></td>";
+                        echo "<td><a href='comments.php?unapprove={$comment_id}'>Unapprove</a></td>";
                         echo "<td><a href='comments.php?delete={$comment_id}'>Delete</a></td>";
                         echo "</tr>";
                     }
@@ -77,8 +77,25 @@
                     </tbody>
                 </table>
 
-
                 <?php
+                if(isset($_GET['approve'])){
+
+                    $the_comment_id = $_GET['approve'];
+                    $query = "UPDATE comments SET comment_status = 'approved' WHERE comment_id = {$the_comment_id}";
+                    $approve_comment_query = mysqli_query($connection,$query);
+
+                    header("location: comments.php");
+                }
+
+                if(isset($_GET['unapprove'])){
+
+                    $the_comment_id = $_GET['unapprove'];
+                    $query = "UPDATE comments SET comment_status = 'unapproved' WHERE comment_id = {$the_comment_id}";
+                    $unapprove_comment_query = mysqli_query($connection,$query);
+
+                    header("location: comments.php");
+                }
+
                 if(isset($_GET['delete'])){
 
                     $the_comment_id = $_GET['delete'];
@@ -86,7 +103,6 @@
                     $delete_comment_query = mysqli_query($connection,$query);
 
                     header("location: comments.php");
-
                 }
 
                 ?>
