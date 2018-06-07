7f8248f4a8ab4016d763a23f6d16e0d9bc2dfce1
RustyJonas <rusty.letmaku28@gmail.com>
2018-06-07 15:11:58 +0200

    Displaying User Values in Edit Page

--- cms/admin/includes/edit_user.php
+++ cms/admin/includes/edit_user.php
@@ -2,14 +2,27 @@
 
 <?php
 
-if(isset($_GET['edit_user'])){
+if(isset($_GET['edit_user'])) {
 
     $the_user_id = $_GET['edit_user'];
-    echo $the_user_id;
+
+    $query = "SELECT * FROM users WHERE user_id = $the_user_id";
+    $select_users_query = mysqli_query($connection, $query);
+
+    while ($row = mysqli_fetch_assoc($select_users_query)) {
+        $user_id = $row['user_id'];
+        $username = $row['username'];
+        $user_password = $row['user_password'];
+        $user_firstname = $row['user_firstname'];
+        $user_lastname = $row['user_lastname'];
+        $user_email = $row['user_email'];
+        $user_image = $row['user_image'];
+        $user_role = $row['user_role'];
+        $randSalt = $row['randSalt'];
+
+    }
 }
-
 if(isset($_POST['edit_user'])){
-
     $user_firstname = $_POST['user_firstname'];
     $user_lastname = $_POST['user_lastname'];
     $user_role = $_POST['user_role'];
@@ -37,12 +50,12 @@ if(isset($_POST['edit_user'])){
 
     <div class="form-group">
         <label for="user_firstname">Firstname</label>
-        <input type="text" class="form-control" name="user_firstname">
+        <input type="text" value="<?php echo $user_firstname?>" class="form-control" name="user_firstname">
     </div>
 
     <div class="form-group">
         <label for="user_lastname">Lastname</label>
-        <input type="text" class="form-control" name="user_lastname">
+        <input type="text" value="<?php echo $user_lastname?>" class="form-control" name="user_lastname">
     </div>
 
     <div class="form-group">
@@ -50,7 +63,6 @@ if(isset($_POST['edit_user'])){
             <option value="Subscriber">Select Options</option>
             <option value="Admin">Admin</option>
             <option value="Subscriber">Subscriber</option>
-
         </select>
     </div>
 
@@ -60,17 +72,17 @@ if(isset($_POST['edit_user'])){
 
     <div class="form-group">
         <label for="username">Username</label>
-        <input type="text" class="form-control" name="username">
+        <input value="<?php echo $username?>" type="text" class="form-control" name="username">
     </div>
 
     <div class="form-group">
         <label for="user_email">Email</label>
-        <input type="text" class="form-control" name="user_email">
+        <input type="text" value="<?php echo $user_email?>" class="form-control" name="user_email">
     </div>
 
     <div class="form-group">
         <label for="user_password">Password</label>
-        <input type="password" class="form-control" name="user_password">
+        <input type="password" class="form-control" value="<?php echo $user_password?>" name="user_password">
     </div>
 
     <div class="form-group">
