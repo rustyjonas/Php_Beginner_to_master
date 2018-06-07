a62c1f90998a7d25078802efbd368837eea6e98b
RustyJonas <rusty.letmaku28@gmail.com>
2018-06-07 14:37:38 +0200

    Deleting users

--- cms/admin/includes/view_all_users.php
+++ cms/admin/includes/view_all_users.php
@@ -72,9 +72,9 @@
 //
 //                        }
                         echo "<td></td>";
-                        echo "<td><a href='comments.php?approve='>Approve</a></td>";
-                        echo "<td><a href='comments.php?unapprove='>Unapprove</a></td>";
-                        echo "<td><a href='comments.php?delete='>Delete</a></td>";
+                        echo "<td><a href='comments.php?approve={$user_id}'>Approve</a></td>";
+                        echo "<td><a href='comments.php?unapprove={$user_id}'>Unapprove</a></td>";
+                        echo "<td><a href='users.php?delete={$user_id}'>Delete</a></td>";
                         echo "</tr>";
                     }
                     ?>
@@ -102,11 +102,11 @@
 
                 if(isset($_GET['delete'])){
 
-                    $the_comment_id = $_GET['delete'];
-                    $query = "DELETE FROM comments WHERE comment_id = {$the_comment_id}";
-                    $delete_comment_query = mysqli_query($connection,$query);
+                    $the_user_id = $_GET['delete'];
+                    $query = "DELETE FROM users WHERE user_id = {$the_user_id}";
+                    $delete_user_query = mysqli_query($connection,$query);
 
-                    header("location: comments.php");
+                    header("location: users.php");
                 }
 
                 ?>
