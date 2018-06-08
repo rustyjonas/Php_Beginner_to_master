7b065d447cdfb5f9dd64caf2c6d9e2341604919b
RustyJonas <rusty.letmaku28@gmail.com>
2018-06-08 09:30:18 +0200

    Creating the login form

--- cms/includes/sidebar.php
+++ cms/includes/sidebar.php
@@ -4,7 +4,7 @@
     <!-- Blog Search Well -->
     <div class="well">
         <h4>Blog Search</h4>
-        <form action="" method="post">
+        <form action="search.php" method="post">
         <div class="input-group">
             <input name="search" type="text" class="form-control">
             <span class="input-group-btn">
@@ -16,6 +16,25 @@
         </form><!--search form-->
         <!-- /.input-group -->
     </div>
+
+    <!-- Login -->
+    <div class="well">
+        <h4>Blog Search</h4>
+        <form action="includes/login.php" method="post">
+            <div class="form-group">
+                <input  name="username" type="text" class="form-control" placeholder="Enter Username">
+            </div>
+
+            <div class="input-group">
+                <input  name="password" type="password" class="form-control" placeholder="Enter Password">
+                <span class="input-group-btn">
+                    <button class="btn btn-primary" type="submit" name="login">Submit</button>
+                </span>
+            </div>
+        </form><!--search form-->
+        <!-- /.input-group -->
+    </div>
+
 
 
 
