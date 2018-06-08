d964eb129509b6b9b41798a2eab1f1af4025a276
RustyJonas <rusty.letmaku28@gmail.com>
2018-06-08 12:38:09 +0200

    Adjusting Widget Links

--- cms/admin/index.php
+++ cms/admin/index.php
@@ -47,7 +47,7 @@
                                     </div>
                                 </div>
                             </div>
-                            <a href="posts.php">
+                            <a href="./posts.php">
                                 <div class="panel-footer">
                                     <span class="pull-left">View Details</span>
                                     <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
@@ -74,7 +74,7 @@
                                     </div>
                                 </div>
                             </div>
-                            <a href="comments.php">
+                            <a href="./comments.php">
                                 <div class="panel-footer">
                                     <span class="pull-left">View Details</span>
                                     <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
@@ -101,7 +101,7 @@
                                     </div>
                                 </div>
                             </div>
-                            <a href="users.php">
+                            <a href="./users.php">
                                 <div class="panel-footer">
                                     <span class="pull-left">View Details</span>
                                     <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
@@ -128,7 +128,7 @@
                                     </div>
                                 </div>
                             </div>
-                            <a href="categories.php">
+                            <a href="./categories.php">
                                 <div class="panel-footer">
                                     <span class="pull-left">View Details</span>
                                     <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
