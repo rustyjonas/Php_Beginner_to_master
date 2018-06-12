73c66ef5dc30457d35570030f124e5092a44171a
RustyJonas <rusty.letmaku28@gmail.com>
2018-06-12 14:52:52 +0200

    Adding Validation to Comments in Post Page

--- cms/admin/includes/admin_navigation.php
+++ cms/admin/includes/admin_navigation.php
@@ -16,7 +16,18 @@
         <li><a href="../index.php">HOME SITE</a></li>
 
         <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> John Smith <b class="caret"></b></a>
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i>
+
+                <?php
+
+                if(isset($_SESSION['username'])){
+
+                    echo $_SESSION['username'];
+
+                }
+
+                ?>
+                <b class="caret"></b></a>
             <ul class="dropdown-menu">
                 <li>
                     <a href="#"><i class="fa fa-fw fa-user"></i> Profile</a>
@@ -32,7 +43,7 @@
     </ul>
 
 
-    <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+    <!-- Sidebar Menu Items - Thes  e collapse to the responsive navigation menu on small screens -->
     <div class="collapse navbar-collapse navbar-ex1-collapse">
         <ul class="nav navbar-nav side-nav">
             <li>
