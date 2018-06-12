9519b5de517c08049b45ddf129392337fce0a587
RustyJonas <rusty.letmaku28@gmail.com>
2018-06-12 15:22:13 +0200

    Extracting Form Values and Escaping

--- cms/registration.php
+++ cms/registration.php
@@ -2,8 +2,23 @@
  <?php  include "includes/header.php"; ?>
 
 
+<?php
+
+if(isset($_POST['submit'])){
+
+    $username = $_POST['username'];
+    $email    = $_POST['email'];
+    $password = $_POST['password'];
+
+    $username = mysqli_real_escape_string($connection,$username);
+    $email    = mysqli_real_escape_string($connection,$email);
+    $password = mysqli_real_escape_string($connection,$password);
+
+}
+
+?>
+
     <!-- Navigation -->
-    
     <?php  include "includes/navigation.php"; ?>
     
  
