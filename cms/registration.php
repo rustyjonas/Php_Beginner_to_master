9b4fd5af541cf41dde0bb07c48a851f3316925d3
RustyJonas <rusty.letmaku28@gmail.com>
2018-06-15 12:55:24 +0200

    Explanation of new New function and Implementation

--- cms/registration.php
+++ cms/registration.php
@@ -15,17 +15,20 @@ if(isset($_POST['submit'])){
         $email    = mysqli_real_escape_string($connection,$email);
         $password = mysqli_real_escape_string($connection,$password);
 
-        $query = "SELECT randSalt FROM users";
-        $select_randsalt_query = mysqli_query($connection, $query);
-        if(!$select_randsalt_query) {
-            die("Query Failed" . mysqli_error($connection));
-        }
+        $password = password_hash($password, PASSWORD_DEFAULT, array('cost' => 12) );
 
-        $row = mysqli_fetch_array($select_randsalt_query);
 
-        $salt = $row['randSalt'];
-
-        $password = crypt($password, $salt);
+//        $query = "SELECT randSalt FROM users";
+//        $select_randsalt_query = mysqli_query($connection, $query);
+//        if(!$select_randsalt_query) {
+//            die("Query Failed" . mysqli_error($connection));
+//        }
+//
+//        $row = mysqli_fetch_array($select_randsalt_query);
+//
+//        $salt = $row['randSalt'];
+//
+//        $password = crypt($password, $salt);
 
         $query = "INSERT INTO users (username, user_password, user_email, user_role) VALUES ('{$username}','{$password}','{$email}','Subscriber')";
         $register_user_query = mysqli_query($connection,$query);
---
+++ cms/test.php
@@ -0,0 +1,8 @@
+<?php
+
+
+
+echo password_hash('secret', PASSWORD_DEFAULT, array('cost' => 10) );
+
+
+?>;
