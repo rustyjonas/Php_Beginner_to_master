c79e851c7fc1a9bcdcc76c769db282f781fc8343
RustyJonas <rusty.letmaku28@gmail.com>
2018-06-15 15:36:15 +0200

    Comment count and amount display

--- cms/admin/includes/view_all_post.php
+++ cms/admin/includes/view_all_post.php
@@ -158,7 +158,13 @@ if(isset($_POST['checkBoxArray'])) {
                             echo "<td><img width='100' class='img-responsive' src='image/$post_image' alt='images'></td>";
                             echo "<td>$post_content</td>";
                             echo "<td>$post_tags</td>";
-                            echo "<td>$post_comment_count</td>";
+
+                            $query = "SELECT * FROM comments WHERE comment_post_id = $post_id";
+                            $send_comment_query = mysqli_query($connection, $query);
+                            $count_comments = mysqli_num_rows($send_comment_query);
+
+
+                            echo "<td>$count_comments</td>";
                             echo "<td>$post_date</td>";
                             echo "<td><a href='../post.php?p_id={$post_id}'>View Post</a></td>";
                             echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
