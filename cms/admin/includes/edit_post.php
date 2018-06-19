9ee824c5868acfb77dfc49f6ed46d2677968277b
RustyJonas <rusty.letmaku28@gmail.com>
2018-06-19 14:59:00 +0200

    Duplicate category improvement

--- cms/admin/includes/edit_post.php
+++ cms/admin/includes/edit_post.php
@@ -86,7 +86,14 @@ while($row = mysqli_fetch_assoc($select_posts_by_id)) {
                                 $cat_id = $row['cat_id'];
                                 $cat_title = $row['cat_title'];
 
-                                echo "<option value='{$cat_id}'>{$cat_title}</option>";
+                                if($cat_id == $post_category_id){
+
+                                    echo "<option selected value='{$cat_id}'>{$cat_title}</option>";
+
+                                } else{
+                                    echo "<option value='{$cat_id}'>{$cat_title}</option>";
+
+                                }
 
                             }
                             ?>
