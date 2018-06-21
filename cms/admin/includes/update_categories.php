547e8fa1cec856ed161fb312df84489e8070024e
RustyJonas <rusty.letmaku28@gmail.com>
2018-06-21 12:08:03 +0200

    Updating categories with prepare statements

--- cms/admin/includes/update_categories.php
+++ cms/admin/includes/update_categories.php
@@ -25,14 +25,19 @@
 
             $the_cat_title = $_POST['cat_title'];
 
-            $query = "UPDATE categories SET cat_title =  '{$the_cat_title}' WHERE cat_id = {$cat_id} ";
-            $update_query = mysqli_query($connection,$query);
+            $stmt = mysqli_prepare($connection, "UPDATE categories SET cat_title = ? WHERE cat_id = ?");
 
-            if(!$update_query){
+            mysqli_stmt_bind_param($stmt, 'si',$the_cat_title, $cat_id);
+
+            mysqli_stmt_execute($stmt);
+
+            if(!$stmt){
 
                 die("QUERY FAILED" . mysqli_error($connection));
 
             }
+
+            redirect("categories.php");
         }
 
         ?>
